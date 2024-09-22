70c5b547492d5c8cc56b2ce52aa9ee91e2b60fe8
tarantino19 <tolentinored19@gmail.com>
2024-09-22 15:03:34 +0800

    change loader styling

--- client/src/components/Loader.tsx
+++ client/src/components/Loader.tsx
@@ -6,15 +6,16 @@ const Loader = () => {
 				justifyContent: 'center',
 				alignItems: 'center',
 				height: '100vh', // Full viewport height
+				background: 'linear-gradient(to right, #2ef3a4, #0072ff)', // Green to blue gradient
 			}}
 		>
 			<div
 				style={{
-					border: '4px solid #f472b6',
-					borderTop: '4px solid transparent',
-					borderRadius: '50%',
-					width: '64px',
-					height: '64px',
+					border: '8px solid rgba(255, 255, 255, 0.3)', // Semi-transparent border
+					borderTop: '8px solid white', // Solid white for contrast
+					borderRadius: '50%', // Ensures it's a circle
+					width: '15vw', // 15% of viewport width
+					height: '15vw', // Equal to the width to ensure it's a circle
 					animation: 'spin 1s linear infinite',
 				}}
 			></div>
